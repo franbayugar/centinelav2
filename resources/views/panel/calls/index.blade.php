2c569fa4d29a322a088f2036b265382295904193
facubinetti <facubinetti.96@gmail.com>
2021-10-13 13:55:55 -0300

    cambio estilo

--- resources/views/panel/calls/index.blade.php
+++ resources/views/panel/calls/index.blade.php
@@ -57,10 +57,15 @@
 
                             @if ($call->notified == 1)
                                 
-                                    <td class="text-center">  <button type="button" class="btn btn-primary"> Ok </button>  </td>
+                                    <td class="text-center">  <button type="button" class="btn btn-success" disabled> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
+                                        <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z"/>
+                                        <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z"/>
+                                      </svg>   </button>  </td>
                                 
                            @else
-                                <td class="text-center"> <a href="{!! route('calls.updateNotified', $call->id) !!}" class="btn btn-danger" > x </a></td>
+                                <td class="text-center"> <a href="{!! route('calls.updateNotified', $call->id) !!}" class="btn btn-primary" > <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                  </svg>    </td>
                            @endif 
                            
                            
